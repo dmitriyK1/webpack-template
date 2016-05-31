a51d364709290024bdacc9edcb5666b609684b78
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2016-06-01 00:53:13 +0300

    up

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,18 @@
 import component from './js/component';
 
-console.log( component() );
+console.log(component());
 
 require('./css/main.css');
 
+async function sayHi() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(777), 3000);
+  });
+}
 
+async function speak() {
+  var result = await sayHi();
+  console.log(result);
+}
+
+speak();
